eb4b3671c16a120001cf1cee0111391e796f510e
AlexYu01 <tengalexyu@gmail.com>
2019-01-31 00:16:06 +0000

    Commit task 11: Summon zombies

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,40 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int infinite_while(void);
+
+/**
+ * infinite_while - Infinite loop.
+ *
+ * Return: 0
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - The main function that will create zombie processes..
+ *
+ * Return: EXIT_SUCCESS.
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 0; i < 5; i++)
+	{
+		if (fork() == 0)
+		{
+			printf("Zombie process created, PID: %i\n", getpid());
+			_exit(EXIT_SUCCESS);
+		}
+	}
+
+	return (infinite_while());
+}
