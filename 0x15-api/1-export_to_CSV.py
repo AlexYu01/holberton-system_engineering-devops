2c630fdfbb36ea5c188ec6ef46e2cb6bc83dfb54
AlexYu01 <tengalexyu@gmail.com>
2019-05-06 23:00:12 +0000

    Dump employee's username rather than their name into CSV file

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -11,22 +11,21 @@ import sys
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
+        user_d = {}
+        todo_l = []
         url = 'https://jsonplaceholder.typicode.com/'
         user_resp = requests.get(url + 'users/{}'.format(sys.argv[1]))
         todo_resp = requests.get(url + 'todos?userId={}'.format(sys.argv[1]))
         if all([user_resp, todo_resp]) is not None:
             try:
                 user_d = json.loads(user_resp.text)
-            except JSONDecodeError:
-                user_d = {}
-            try:
                 todo_l = json.loads(todo_resp.text)
             except JSONDecodeError:
-                todo_l = []
+                pass
             if type(todo_l) is list and type(user_d) is dict and user_d != {}:
                 with open(sys.argv[1] + '.csv', 'w', newline='') as csvfile:
                     write_file = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
                     for todo in todo_l:
-                        line = [user_d.get('id'), user_d.get('name'),
+                        line = [user_d.get('id'), user_d.get('username'),
                                 todo.get('completed'), todo.get('title')]
                         write_file.writerow(line)
