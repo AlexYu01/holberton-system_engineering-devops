bd5804b4bb50ebc7e633a4cf4950a35d879cb292
AlexYu01 <tengalexyu@gmail.com>
2019-05-22 10:12:36 -0700

    Eliminate restart command

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,9 +1,5 @@
 # Fix a typoe within wp-settings.php
 exec { 'Fix typo':
-  command => 'sudo sed -i \'s/.phpp/.php/g\' /var/www/html/wp-settings.php',
-  path    => ['/bin/', '/usr/bin', '/usr/sbin'],
-}->
-exec { 'Restart apache':
-  command => 'sudo service apache2 restart',
+  command => 'sed -i \'s/.phpp/.php/g\' /var/www/html/wp-settings.php',
   path    => ['/bin/', '/usr/bin', '/usr/sbin'],
 }
