ab430d83c8d46d3ef49333e5e933901bd4ba8cae
AlexYu01 <tengalexyu@gmail.com>
2019-05-22 17:11:56 +0000

    Unwrap line

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,6 @@
 # Fix a typoe within wp-settings.php
 exec { 'Fix typo':
-  command => 'sudo sed -i \'s/class-wp-locale.phpp/class-wp-locale.php/g\' \
-/var/www/html/wp-settings.php',
+  command => 'sudo sed -i \'s/.phpp/.php/g\' /var/www/html/wp-settings.php',
   path    => ['/bin/', '/usr/bin', '/usr/sbin'],
 }->
 exec { 'Restart apache':
