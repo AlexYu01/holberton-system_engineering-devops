93a7526683f375c4553738db3b3843d630da1434
AlexYu01 <tengalexyu@gmail.com>
2019-05-22 10:32:51 -0700

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Fix a typo within wp-settings.php
 exec { 'fix-wordpress':
   command => 'sed -i \'s/.phpp/.php/\' /var/www/html/wp-settings.php',
-  path    => ['/bin', '/usr/bin', '/usr/sbin'],
+  path    => ['/bin', '/usr/bin', '/usr/sbin']
 }
