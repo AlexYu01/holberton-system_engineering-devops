3689cdda19e9c2f23e68e870efafea77f5a094ba
AlexYu01 <tengalexyu@gmail.com>
2019-05-22 10:24:08 -0700

    Remove global

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Fix a typo within wp-settings.php
 exec { 'Fix typo':
-  command => 'sudo sed -i \'s/.phpp/.php/g\' /var/www/html/wp-settings.php',
+  command => 'sudo sed -i \'s/.phpp/.php/\' /var/www/html/wp-settings.php',
   path    => ['/bin/', '/usr/bin', '/usr/sbin'],
 }
