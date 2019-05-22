e78a02d6a409aff053b48cf6d98c5839db3a30ed
AlexYu01 <tengalexyu@gmail.com>
2019-05-22 17:00:04 +0000

    Add sudo power

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -3,7 +3,7 @@ file { '/var/www/html/wp-settings.php':
   ensure => present,
 }->
 exec { 'Fix typo':
-  command => 'sed -i \'s/class-wp-locale.phpp/class-wp-locale.php/g\' \
+  command => 'sudo sed -i \'s/class-wp-locale.phpp/class-wp-locale.php/g\' \
 /var/www/html/wp-settings.php',
   path    => ['/bin/', '/usr/bin', '/usr/sbin'],
 }
