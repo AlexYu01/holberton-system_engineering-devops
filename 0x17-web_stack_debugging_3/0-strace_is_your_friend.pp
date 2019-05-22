098f46272972e87934aa687a57dd45d570fbcab9
AlexYu01 <tengalexyu@gmail.com>
2019-05-22 10:28:54 -0700

    change comment to 'fix-wordpress'

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Fix a typo within wp-settings.php
-exec { 'Fix typo':
-  command => 'sudo sed -i \'s/.phpp/.php/\' /var/www/html/wp-settings.php',
-  path    => ['/bin/', '/usr/bin', '/usr/sbin'],
+exec { 'fix-wordpress':
+  command => 'sed -i \'s/.phpp/.php/\' /var/www/html/wp-settings.php',
+  path    => ['/bin', '/usr/bin', '/usr/sbin'],
 }
