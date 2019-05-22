f7c20ef35112d01e262e01a72c397d997b8bc75f
AlexYu01 <tengalexyu@gmail.com>
2019-05-22 10:10:17 -0700

    Eliminate file check

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,4 @@
 # Fix a typoe within wp-settings.php
-file { '/var/www/html/wp-settings.php':
-  ensure => present,
-}->
 exec { 'Fix typo':
   command => 'sudo sed -i \'s/class-wp-locale.phpp/class-wp-locale.php/g\' \
 /var/www/html/wp-settings.php',
